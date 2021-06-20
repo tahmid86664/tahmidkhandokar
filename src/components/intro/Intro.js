6f9cc6269fcb1d5c6615ddb66dc6afb34ad0dce1
Tahmid Khandokar <tahmidkhandokar82@gmail.com>
2021-06-20 22:19:50 +0600

    designed intro section

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { init } from 'ityped';
 
 import './Intro.scss';
 
 const Intro = () => {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      strings: ["Developer", "Designer", "Debuger"],
+      backDelay: 1700,
+      showCursor: true
+    })
+  }, []);
+
   return (
     <div className='intro' id="intro">
-      <h2>Intro</h2>
+      <div className="intro__left">
+        <div className="intro__imageContainer">
+          <img src="assets/myphoto2.png" alt="" />
+        </div>
+      </div>
+      <div className="intro__right">
+        <div className="intro__rightWrapper">
+          <h2>Hi there, I'm</h2>
+          <h1>Kh. Md. Mustafa Tahmid</h1>
+          <h3>Software <span ref={textRef}></span></h3>
+          <a href="#portfolio">
+            <img src="assets/arrow.png" alt="arrow" />
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
